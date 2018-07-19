4369945879482a0f53958d2a96fcdd5eb8017538
Andrew Fulton <rafulton1984@gmail.com>
2018-07-19 10:43:53 -0400

    Thursday morning changes

--- m3-w3d3-csharp-capstone/Capstone.Web/DAL/ParkSQLDAL.cs
+++ m3-w3d3-csharp-capstone/Capstone.Web/DAL/ParkSQLDAL.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿using Capstone.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
 
 namespace Capstone.Web.DAL
 {
-    public class ParkSQLDAL
+    public class ParkSQLDAL : IParkSqlDAL
     {
+        private readonly string connectionString;
+        private const string sqlPark = "select parkName, state, parkDescription FROM park";
 
+        public ParkSQLDAL(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        public IList<Park> GetAllParks()
+        {
+            IList<Park> parks = new List<Park>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(sqlPark, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Park park = new Park();
+                        park.ParkName = Convert.ToString(reader["parkName"]);
+                        park.State = Convert.ToString(reader["state"]);
+                        park.ParkDescription = Convert.ToString(reader["parkDescription"]);
+
+                        parks.Add(park);
+                    }
+                }
+                return parks;
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+        }
     }
 }
+
+
+
+
+
